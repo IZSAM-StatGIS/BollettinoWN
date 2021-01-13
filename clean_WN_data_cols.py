d8feaa3ff7f8f927b0262b699c10fa0dcb3bf1aa
alessio <alessio.dl@gmail.com>
2021-01-13 16:40:41 +0100

    Aggiunto script per la generazione dei dataset spaziali aggregati

---
+++ clean_WN_data_cols.py
@@ -0,0 +1,32 @@
+import datetime as dt
+import pandas as pd
+import locale
+
+def clean(df):
+
+    "DataSospetto","IdFocolaio","CodIstat","Specie","Categorie","Sierotipo","Regione","Prov","Comune"
+
+  # Rimozione colonne inutili
+    drop_fields = [
+        'STATO_DEL_RECORD','Malattia',
+        'AnnoFocolaioMinsal', 'NumFocolaioMinsal', 'CodSierotipo',
+        'NumCapiAbbattuti', 'NumCapiConSintomi', 'NumCapiDistrutti', 
+        'NumCapiGuariti', 'NumCapiMalati', 'NumCapiMorti', 'NumCapiPresenti', 'TotaleMorti'
+    ]
+    
+    df.drop(drop_fields, axis = 1, inplace=True)
+    
+    # Modifica datatype di CodIstat in stringa
+    df['CodIstat'] = df['CodIstat'].astype(int)
+
+    # Trasformazione in data da stringa DD-MMM-YY a YYYY-MM-DD
+    locale.setlocale(locale.LC_ALL, 'it_IT')
+    df['DataSospetto'] = pd.to_datetime(df['DataSospetto'], format='%d-%b-%y')
+    df['DataConferma'] = pd.to_datetime(df['DataConferma'], format='%d-%b-%y')
+
+    # Seleziona solo i focolai confermati e i campi di interesse per l'aggregazione e l'incrocio con i centroidi
+    df = df.query('DataConferma != "NaT"')[["DataSospetto","IdFocolaio","CodIstat","Specie","Categorie","Sierotipo","Regione","Prov","Comune"]]
+
+    return df
+
+
---
+++ run.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import geopandas as gpd
+import os
+from clean_WN_data_cols import clean
+
+# Dati spaziali (Centroidi dei Comuni dalla BDN)
+gdf_comuni = gpd.read_file(r"input/shp/centroidi_comuni_bdn.shp")[['ISTAT','geometry']]
+gdf_comuni.rename(columns={'ISTAT':'CodIstat'}, inplace=True)
+gdf_comuni["CodIstat"] = gdf_comuni["CodIstat"].astype(int)
+
+# ##########################################################
+# WEST NILE DISEASE
+# ##########################################################
+
+# 1 - Importazione e pulizia dati excel
+df_wn = pd.read_excel(r'input/xls/wn.xlsx')
+df_wn = clean(df_wn)
+df_wn = df_wn.query("DataSospetto >= '2008-01-01' & DataSospetto <= '2018-12-31'").sort_values(by=["DataSospetto"])
+# 2 - Aggregazione e merge con i centroidi dei comuni
+df_wn_group = df_wn.groupby(['DataSospetto','CodIstat','Categorie','Regione','Prov','Comune'])["CodIstat"].count().reset_index(name="count")
+distribuzione_wn = gdf_comuni.merge(df_wn_group, on='CodIstat')
+# 3 - Esportazione
+distribuzione_wn.to_file(r"output/distr_wn_centroidi.geojson", driver='GeoJSON')
+
+# ##########################################################
+# USUTU VIRUS
+# ##########################################################
+
+# 1 - Importazione dati excel
+df_usutu = pd.read_excel(r"input/xls/usutu.xlsx")
+# 2 - Aggregazione e conteggio delle due categorie previste 
+df_insetti = df_usutu.query("SpecieCampione == 'CULEX PIPIENS'")
+df_uccelli = df_usutu.query("SpecieCampione == 'UCCELLO SELVATICO'")
+
+df_insetti_group = df_insetti.groupby(['Anno','Sede','Provincia','Comune'])["Comune"].count().reset_index(name="N_insetti")
+df_uccelli_group = df_uccelli.groupby(['Anno','Sede','Provincia','Comune'])["Comune"].count().reset_index(name="N_uccelli")
+
+df_usutu_group = df_insetti_group.append(df_uccelli_group).fillna(0)
+df_usutu_group['N_insetti'] = df_usutu_group['N_insetti'].astype(int)
+df_usutu_group['N_uccelli'] = df_usutu_group['N_uccelli'].astype(int)
+
+df_usu = df_usutu_group.groupby(['Anno','Sede','Provincia','Comune'],as_index=False).agg('sum')
+# df_us.query('N_insetti > 0 & N_uccelli > 0')
+
+# 3 - Merge con i centroidi dei comuni
+# To-Do... manca codice istat in dati usutu di partenza
+
+# 4 - Esportazione
+# To-Do... temporanemente esporta csv
+df_usu.to_csv(r"output/distr_usu_centroidi.csv", index=False)
+
+
